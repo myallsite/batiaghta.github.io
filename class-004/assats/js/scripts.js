4fa352c628e65672a95d070ecb86a6bdf0587835
Raju <myallsite9@gmail.com>
2017-08-23 20:10:48 +0600

    class-four

---
+++ class-004/assats/js/scripts.js
@@ -0,0 +1,37 @@
+jQuery(document).ready(function($) {
+	$(".homepage-slides").owlCarousel({
+		items: 1,
+		loop: true,
+		nav: true,
+		dots: true,
+		navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
+		autoplay: false
+	});
+	
+	$("#web-design-skillbar").circleProgress({
+		value: 0.90,
+		size: 270,
+		thickness: 3,
+		fill:'#0FBCC2',
+	}).on('circle-animation-progress', function(event, progress) {
+		$(this).find('.skil-count-no').html(Math.round(90 * progress) + '<i>%</i>');
+    });
+		
+	$("#graphic-design-skillbar").circleProgress({
+		value: 0.95,
+		size: 270,
+		thickness: 3,
+		fill:'#0FBCC2',
+	}).on('circle-animation-progress', function(event, progress) {
+		$(this).find('.skil-count-no').html(Math.round(95 * progress) + '<i>%</i>');
+    });
+		
+	$("#digital-marketing-skillbar").circleProgress({
+		value: 0.84,
+		size: 270,
+		thickness: 3,
+		fill:'#0FBCC2',
+	}).on('circle-animation-progress', function(event, progress) {
+		$(this).find('.skil-count-no').html(Math.round(84 * progress) + '<i>%</i>');
+    });
+});
